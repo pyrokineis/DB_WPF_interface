ef00dfafaf459e551f08174c41bad2d1635cefaf
Andrew Kalmykov <kadqwq@gmail.com>
2020-09-17 17:34:11 +0300

    sen 17 17:30

--- Kursach/Windows/ClientWindow.xaml.cs
+++ Kursach/Windows/ClientWindow.xaml.cs
@@ -43,12 +43,18 @@ namespace Kursach.Windows
         public int ClientID { get; set; }
         public SqlConnection connect { get; set; }
         SqlCommand cmnd, cmndFind;
-        SqlDataReader Reader;
+        SqlDataReader reader;
         Client client;
-        SqlDataAdapter adapter;
         DataTable DT;
+        int rDriver, driverCount, DriverID, serviseID,classID;
+        string PayOperator,payType;
 
         List<string> CtTableStrings = new List<string> { "Client_ID", "C_Full_name", "C_Age", "C_Phone_Number" };
+        List<string> DriversIDs = new List<string> { };
+        List<string> PayTypeString = new List<string> { };
+        List<string> PayOpsString = new List<string> { };
+        List<string> AutoClassString = new List<string> { };
+        List<string> ExtraServString = new List<string> { };
         public ClientWindow()
         {
             InitializeComponent();
@@ -69,14 +75,14 @@ namespace Kursach.Windows
         private void LoadClientInfo(int id)
         {
             cmnd.CommandText = "select * from Client where Client_ID =" + id;
-            Reader = cmnd.ExecuteReader();
-            if (Reader.Read())
+            reader = cmnd.ExecuteReader();
+            if (reader.Read())
             {
                 client = new Client(
-                    (Reader[0].ToString()),
-                    Reader[1].ToString(),
-                    Reader[2].ToString(),
-                    Reader[3].ToString()
+                    (reader[0].ToString()),
+                    reader[1].ToString(),
+                    reader[2].ToString(),
+                    reader[3].ToString()
                     );
             }
 
@@ -86,7 +92,7 @@ namespace Kursach.Windows
                 Close();
 
             }
-            Reader.Close();
+            reader.Close();
 
 
         }
@@ -102,18 +108,58 @@ namespace Kursach.Windows
             TableGrid.Visibility = Visibility.Hidden;
             top_SP.Visibility = Visibility.Hidden;
             top_SP2.Visibility = Visibility.Hidden;
-            //адр123, дист, сумма, дата, айдди вод, айди кл, оп опл, класс авто, доп усл
-            //ввод адр123, оп опл, класс авто, дор усл
-            //обн цены
 
+            cmnd = new SqlCommand("select Driver_ID from Driver",connect);
+            reader = cmnd.ExecuteReader();
+            while (reader.Read())
+            {
+                DriversIDs.Add(reader.GetValue(0).ToString());
+            }
+            reader.Close();
+            cmnd = new SqlCommand("select Pay_Type from Payment", connect);
+            reader = cmnd.ExecuteReader();
+            while (reader.Read())
+            {
+                PayTypeString.Add(reader.GetValue(0).ToString());
+            }
+
+            reader.Close();
+
+            foreach (string i in PayTypeString)
+            {
+                if (i == "None")
+                    PayTypeString[PayTypeString.IndexOf(i)] = "Безналичный";
+
+            }
+
+            cmnd = new SqlCommand("select Class_Name from AutoClass", connect);
+            reader = cmnd.ExecuteReader();
+            while (reader.Read())
+            {
+                AutoClassString.Add(reader.GetValue(0).ToString());
+            }
+            reader.Close();
+
+            cmnd = new SqlCommand("select Servise_Name from ExtraServises", connect);
+            reader = cmnd.ExecuteReader();
+            while (reader.Read())
+            {
+                ExtraServString.Add(reader.GetValue(0).ToString());
+            }
+            reader.Close();
+
+            AutoClass_TB.ItemsSource = AutoClassString;
+            PayType_CB.ItemsSource = PayTypeString;
+            ExtraServ_CB.ItemsSource = PayTypeString;
         }
+
         private void Btn_History_Click(object sender, RoutedEventArgs e)
         {
             TableGrid.Visibility = Visibility.Visible;
             top_SP.Visibility = Visibility.Visible;
             top_SP2.Visibility = Visibility.Visible;
         }
-        private void Btn_Find_Click(object sender, RoutedEventArgs e)
+        private void Btn_Find_Click(object sender, RoutedEventArgs e) //dodelat
         {
         
 
@@ -157,6 +203,89 @@ namespace Kursach.Windows
         private void Btn_Report_Click(object sender, RoutedEventArgs e)
         {
 
+        }
+
+        private void PlaceOrder_Click(object sender, RoutedEventArgs e)
+        {
+            //адр123, дист, сумма, дата, айдди вод, айди кл, оп опл, класс авто, доп усл
+
+            //обн цены
+            if (OrderGrid.Visibility == Visibility.Visible)
+            {
+                driverCount = DriversIDs.Count;
+                Random r = new Random();
+                DriverID = r.Next(driverCount);
+
+              
+
+                if (Adress1_TB.Text != null & Adress3_TB.Text != null & PayType_CB.SelectedItem != null & AutoClass_TB.SelectedItem != null)
+                {
+
+                    if (PayType_CB.SelectedItem.ToString() == "Безналичный")
+                        payType = "None";
+
+                    cmnd = new SqlCommand("select Class_ID from AutoClass where Class_Name="+"'"+AutoClass_TB.SelectedItem.ToString()+"'",connect);
+                    reader = cmnd.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        classID = int.Parse(reader.GetValue(0).ToString());
+                    }
+                    reader.Close();
+
+                    cmnd = new SqlCommand("select Servise_ID from ExtraServises where Servise_Name="+ "'"+ExtraServ_CB.SelectedItem.ToString()+"'",connect);
+                    reader = cmnd.ExecuteReader();
+                    while(reader.Read())
+                    {
+                        serviseID = int.Parse(reader.GetValue(0).ToString());
+                    }
+                    reader.Close();
+                  
+                    try
+                    {
+                        cmnd = new SqlCommand("Insert into Rides (Rides_ID ,Adress1 ,Adress2 ,Adress3 ,Distance ,Summary ,DataTime ,DriverID ,ClientID ,PaymentOperator,autoClass_ID ,ExServise_ID) " +
+                    "values (@Rides_ID, @Adress1, @Adress2, @Adress3, @Distance, @Summary, @DataTime, @DriverID, @ClientID, @PaymentOperator, @autoClass_ID, @ExServise_ID)", connect);
+
+                        //получчть айдишник
+                        //SqlParameter param = new SqlParameter();
+                        //param.ParameterName = "";
+                        //param.Value = ;
+                        //cmmnd.Parameters.Add(param);
+
+                        //SqlParameter param = new SqlParameter();
+                        //param.ParameterName = "";
+                        //param.Value = ;
+                        //cmmnd.Parameters.Add(param);
+                        //SqlParameter param = new SqlParameter();
+                        //param.ParameterName = "";
+                        //param.Value = ;
+                        //cmmnd.Parameters.Add(param);
+                        //SqlParameter param = new SqlParameter();
+                        //param.ParameterName = "";
+                        //param.Value = ;
+                        //cmmnd.Parameters.Add(param);
+                        //SqlParameter param = new SqlParameter();
+                        //param.ParameterName = "";
+                        //param.Value = ;
+                        //cmmnd.Parameters.Add(param);
+                        //SqlParameter param = new SqlParameter();
+                        //param.ParameterName = "";
+                        //param.Value = ;
+                        //cmmnd.Parameters.Add(param);
+                        //SqlParameter param = new SqlParameter();
+                        //param.ParameterName = "";
+                        //param.Value = ;
+                        //cmmnd.Parameters.Add(param);
+                        //SqlParameter param = new SqlParameter();
+                        //param.ParameterName = "";
+                        //param.Value = ;
+                        //cmmnd.Parameters.Add(param);
+                    }
+                    catch
+                    {
+
+                    }
+                }
+            }
         }
 
         private void Btn_Back_Click(object sender, RoutedEventArgs e)
